5e31deb6a2b360e9eb65b87189cb6975804e014e
K1ngHandy <bolds-slicks0g@icloud.com>
2024-09-03 00:54:00 -0400

    Initial Commit

---
+++ Symbol Effects/Symbol_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Symbol_EffectsApp.swift
+//  Symbol Effects
+//
+//  Created by Steve Handy on 2024.09.03.
+//
+
+import SwiftUI
+
+@main
+struct Symbol_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
